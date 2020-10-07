ccc344befab7ac72d5994528b15f7521de16587d
Dany <daniel.demecko@gmail.com>
2020-10-07 22:10:41 +0000

    fixed footer styling

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -26,7 +26,7 @@ const FOOTER_NAVIGATION = [
 const Footer = () => (
   <footer className="bg-dark text-gray-100 print:bg-white print:text-gray-500">
     <div className="container mx-auto  px-3 flex flex-col md:flex-row-reverse text-center">
-      <nav className="print:hidden mt-5 md:py-5 flex flex-col md:flex-row ">
+      <nav className="print:hidden py-5 flex flex-col md:flex-row ">
         {
           FOOTER_NAVIGATION.map(({link, label}, i) => 
           <Link to={link} activeClassName="text-orange-400" className="text-center md:ml-5 px-3 pb-5 md:pb-0 block md:flex" key={i}>
