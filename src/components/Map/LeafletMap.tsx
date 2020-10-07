c9f4bd16f798a10204b0e0fdbb8b2dec5e8c1313
Dany <daniel.demecko@gmail.com>
2020-10-07 20:59:26 +0000

    new style in contact and outro

--- src/components/Map/LeafletMap.tsx
+++ src/components/Map/LeafletMap.tsx
@@ -7,10 +7,9 @@ import { isMobile } from 'react-device-detect';
 import { Helmet } from 'react-helmet';
 
 const style = {
-    height: "15em",
+    height: "15.3rem",
     width: "100%",
     zIndex: 1,
-
 }
 
 
@@ -27,7 +26,7 @@ interface props {
 }
 
 const icon = new L.Icon({
-  iconUrl: withPrefix("/marker-icon.png"),
+  iconUrl: withPrefix("/marker-icon.svg"),
 });
 
 export class LeafletMap extends Component<props>  {
@@ -48,6 +47,7 @@ export class LeafletMap extends Component<props>  {
             zoomControl={false}
             dragging={false}
             boxZoom={false}
+            style={{height: "100%"}}
           >
             <TileLayer
               attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
