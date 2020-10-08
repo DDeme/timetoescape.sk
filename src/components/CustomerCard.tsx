37f4379e773eeb1c8756a225dd8d4a292486e7ed
Dany <daniel.demecko@gmail.com>
2020-10-08 23:31:05 +0000

    user reviews

--- src/components/CustomerCard.tsx
+++ src/components/CustomerCard.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
 import Card from './Card';
+import StarRatings from 'react-star-ratings';
+import {lightFormat} from 'date-fns'
 export interface Customer {
+  rating: number,
+  scale: number,
+  source: number,
+  date: string,
   customerImage: string,
   customerName: string,
-  customerTitle: string,
+
   title: string,
   content: string,
 }
@@ -12,14 +18,26 @@ interface props {
 }
 
 
+const formatDate = (date: string): string => lightFormat(new Date(date), 'dd.MM.yyyy')
+
 export const CustomerCard = ({ customer }: props) => (
   <Card className="mb-8 p-10">
-    {
-      /*
-    <p className="text-xl font-semibold">{customer.title}</p>
-      */
-  }
-    <q className="mt-6">{customer.content}</q>
+    <div className="mb-8">
+    <StarRatings
+    rating={customer.rating}
+    starDimension="1.5rem"
+      starSpacing=".5rem"
+      starRatedColor="#f6ad55"
+      starEmptyColor="rgba(26, 32, 44,.3)"
+      numberOfStars={customer.scale}
+      name="hodnotenie"
+      />
+      <div className="float-right">
+      {customer.rating}/{customer.scale}
+      </div>
+    </div>  
+    <q>{customer.content}</q>
+    <br/><small>Zdroj:Facebook</small>
     <div className="flex items-center mt-8">
       <img
         className="w-12 h-12 mr-4 rounded-full"
@@ -28,7 +46,7 @@ export const CustomerCard = ({ customer }: props) => (
       />
       <div>
         <p>{customer.customerName}</p>
-        <p className="text-sm">{customer.customerTitle}</p>
+        <p className="text-sm">{formatDate(customer.date)}</p>
       </div>
     </div>
   </Card>
