e8c7290fce36fc57f8dc0f2be640646fac9de922
Dany <daniel.demecko@gmail.com>
2020-10-12 14:46:42 +0200

    Update koronavirus.tsx

--- src/pages/koronavirus.tsx
+++ src/pages/koronavirus.tsx
@@ -6,7 +6,7 @@ export default () => (
   <Layout>
     <section className="my-10">
       <article className="container px-3 mx-auto md:text-lg">
-        <h1 className="mb-10 text-4xl alt-font">
+        <h1 className="mb-10 text-4xl alt-font">Opatrenia prijaté v súlade s aktulánymi nariadeniami</h1>
         <p>Ak máte akékoľvek ďalšie otázky, neváhajte nás kontaktovať.</p>
       </article>
     </section>
