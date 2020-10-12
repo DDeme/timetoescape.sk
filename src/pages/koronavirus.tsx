9fd619dd2f04b766f4c7d3bf20c8f37eae6d3f11
Dany <daniel.demecko@gmail.com>
2020-10-12 14:43:51 +0200

    Create koronavirus.tsx

---
+++ src/pages/koronavirus.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Layout from "../components/layout/Layout";
+
+
+export default () => (
+  <Layout>
+    <section className="my-10">
+      <article className="container px-3 mx-auto md:text-lg">
+        <h1 className="mb-10 text-4xl alt-font">
+        <p>Ak máte akékoľvek ďalšie otázky, neváhajte nás kontaktovať.</p>
+      </article>
+    </section>
+  </Layout>
+)
