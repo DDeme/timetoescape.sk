7d44431fc2e2f04991fb8231ebdb51ba46287771
Dany <daniel.demecko@gmail.com>
2020-10-23 23:43:13 +0000

    notice manifest.json

--- src/components/Notice.tsx
+++ src/components/Notice.tsx
@@ -13,7 +13,7 @@ import {title , description, rules} from '../../content/home/notice.json'
 export const Notice = () => <section id="koronavirus" className="my-20 container mx-auto px-3">
         <h2 className="text-3xl lg:text-4xl alt-font text-center px-3">{title}</h2>
         <div className="mt-10 text-center lg:w-2/3 mx-auto text-justify">
-               <p className="md:text-lg">
+               <p className="md:text-lg text-center">
                  Prevádzka je uzavretá do 1.11.2020.
                </p>
                {/* <p className="md:text-lg ">
