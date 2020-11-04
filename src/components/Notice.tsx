d41b6c73249e6c78ab5c1b1fcfb1a19f04806dcd
Dany <daniel.demecko@gmail.com>
2020-11-04 15:15:13 +0000

    added new coronavirus notice

--- src/components/Notice.tsx
+++ src/components/Notice.tsx
@@ -13,10 +13,8 @@ import {title , description, rules} from '../../content/home/notice.json'
 export const Notice = () => <section id="koronavirus" className="my-20 container mx-auto px-3">
         <h2 className="text-3xl lg:text-4xl alt-font text-center px-3">{title}</h2>
         <div className="mt-10 text-center lg:w-2/3 mx-auto text-justify">
-               <p className="md:text-lg text-center">
-                 Prevádzka je uzavretá do 1.11.2020.
-               </p>
-               {/* <p className="md:text-lg ">
+      
+              <p className="md:text-lg ">
                Naša úniková hra ostáva aj po zavedení núdzového stavu a prijatí opatrení RÚVZ stále <b>OTVORENÁ!</b> Samozrejme za sprísnených hygienických podmienok. Priestory sú pravidelne dezinfikované a nepretržite vetrané.
                </p>
 
@@ -28,7 +26,7 @@ export const Notice = () => <section id="koronavirus" className="my-20 container
                       {v}
                     </li>
                ))}
-            </ol> */}
+            </ol>
              </div>
 </section>
 
