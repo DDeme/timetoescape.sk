bdd040bd1b3126433c6a3b2109d3688fa1c6dfac
Dany <5726983+DDeme@users.noreply.github.com>
2022-10-20 14:45:02 +0200

    fix dependency problems

--- gatsby-config.js
+++ gatsby-config.js
@@ -82,7 +82,7 @@ module.exports = {
         printRejected: true, // Print removed selectors and processed file names
         // develop: true, // Enable while using `gatsby develop`
         tailwind: true, // Enable tailwindcss support
-        ignore: ["react-image-lightbox/style.css"], // Ignore files/folders
+        ignore: ["react-image-lightbox/style.css", "leaflet/dist/leaflet.css"], // Ignore files/folders
         // purgeOnly : ['components/', '/main.css', 'bootstrap/'], // Purge only these files/folders
         purgeCSSOptions: {
           // https://purgecss.com/configuration.html#options
